3c900ba016470f38957768ebe66c1cc210842074
Anton Mitsengendler <a.mic@rtc.ru>
2019-02-14 10:55:38 +0300

    scaffolded phonebookentry model

---
+++ Models/BookEntry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PhoneEdit.Models
+{
+    public partial class BookEntry
+    {
+        [HiddenInput(DisplayValue = false)]
+        public int Id { get; set; }
+        public string PersonnelNumber { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public string Department { get; set; }
+        public string LocalPhoneNumber { get; set; }
+        public string CityPhoneNumber { get; set; }
+        public string Mail { get; set; }
+        public string Room { get; set; }
+        [HiddenInput(DisplayValue = false)]
+        public string Status { get; set; }
+    }
+}
