77a230e13a1d2fe7441458825a1e22ea529183d3
Mikhail Shashin <shashinma@icloud.com>
2023-11-28 10:00:44 +0300

    Init

---
+++ PhoneEdit/Data/SampleData.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace PhoneEdit.Data;
+
+public static class SampleData
+{
+    // Creates default app's admin
+    public static async Task CreateDefaultUser(IServiceProvider serviceProvider)
+    {
+        var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+        var userCheck = await userManager.Users.AnyAsync();
+        if (!userCheck)
+        {
+            var adminUser = new IdentityUser()
+            {
+                UserName = "mail@example.com",
+                Email = "mail@example.com",
+                EmailConfirmed = true
+            };
+
+            await userManager.CreateAsync(adminUser, "123456");
+        }
+    }
+}
