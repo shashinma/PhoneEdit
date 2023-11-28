0e5fb833c62b941e262877e5e1e8eaae88e74db3
Mikhail Shashin <shashinma@icloud.com>
2023-11-28 19:42:47 +0300

    Update PhoneBookController.cs
    Co-Authored-By: akinin <3820354+akinin@users.noreply.github.com>

--- Controllers/PhoneBookController.cs
+++ Controllers/PhoneBookController.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Authorization.Infrastructure;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using PhoneEdit.Data;
 using PhoneEdit.Models;
