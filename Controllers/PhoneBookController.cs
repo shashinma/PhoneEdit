2c6a362038a7c1ac5f65294a0070ad4a09512a7b
Mikhail Shashin <shashinma@icloud.com>
2023-11-30 18:18:26 +0300

    Update PhoneBookController.cs
    Co-Authored-By: akinin <3820354+akinin@users.noreply.github.com>

--- Controllers/PhoneBookController.cs
+++ Controllers/PhoneBookController.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Authorization.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using PhoneEdit.Data;
 using PhoneEdit.Models;
@@ -25,7 +19,7 @@ namespace PhoneEdit.Controllers
 
         // GET: PhoneBook
         [AllowAnonymous]
-        public async Task<IActionResult> Index(string currentFilter, string searchString, int? cPage)
+        public IActionResult Index(string? searchString, string? currentFilter, int? cPage)
         {
             if (searchString != null)
             {
@@ -38,10 +32,8 @@ namespace PhoneEdit.Controllers
 
             ViewBag.CurrentFilter = searchString;
 
-            var entries = from e in _context.Entries
-                select e;
-                    
-                    
+            var entries = _context.Entries.Select(e => e).AsEnumerable();
+
             if (!string.IsNullOrEmpty(searchString))
             {
                 entries = entries.Where(e => e.ToString().Contains(searchString, StringComparison.OrdinalIgnoreCase));
@@ -51,9 +43,10 @@ namespace PhoneEdit.Controllers
 
             int pageSize = 25;
             int pageNumber = (cPage ?? 1);
-            return View((await entries.ToListAsync()).ToPagedList(pageNumber,pageSize));
-        }
 
+            return View(entries.AsQueryable().ToPagedList(pageNumber, pageSize));
+        }
+        
         // GET: PhoneBook/Details/5
         public async Task<IActionResult> Details(int? id)
         {
@@ -114,45 +107,16 @@ namespace PhoneEdit.Controllers
             return View(bookEntry);
         }
 
-        // POST: PhoneBook/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,PersonnelNumber,Name,Position,Department,LocalPhoneNumber,CityPhoneNumber,Mail,Room, Status")] BookEntry bookEntry)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,PersonnelNumber,Name,Position,Department,LocalPhoneNumber,CityPhoneNumber,Mail,Room")] BookEntry bookEntry)
         {
-            if (id != bookEntry.Id)
-            {
-                return NotFound();
-            }
-
-            if (!VerifyPersonnelNumber(bookEntry))
-            {
-                ModelState.AddModelError(nameof(bookEntry.PersonnelNumber), "Табельный номер уже существует");
-            }
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(bookEntry);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!BookEntryExists(bookEntry.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(bookEntry);
+            _context.Entries.Update(bookEntry);
+            await _context.SaveChangesAsync();
+        
+            return RedirectToAction("Index");
         }
-
+        
         // GET: PhoneBook/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
