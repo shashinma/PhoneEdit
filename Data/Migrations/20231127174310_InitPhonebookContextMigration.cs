29ed1d8f07bc64bba1c0ae7ca388551b08172137
Mikhail Shashin <shashinma@icloud.com>
2023-12-05 23:58:56 +0300

    Init migrations

---
+++ Data/Migrations/20231127174310_InitPhonebookContextMigration.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PhoneEdit.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitPhonebookContextMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tel1",
+                columns: table => new
+                {
+                    n = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    tabNumber = table.Column<string>(type: "varchar(5)", nullable: false),
+                    name = table.Column<string>(type: "varchar(250)", nullable: false),
+                    who = table.Column<string>(type: "varchar(250)", nullable: false),
+                    work = table.Column<string>(type: "varchar(250)", nullable: false),
+                    telm = table.Column<string>(type: "varchar(50)", nullable: false),
+                    telg = table.Column<string>(type: "varchar(100)", nullable: false),
+                    mail = table.Column<string>(type: "varchar(250)", nullable: false),
+                    komnata = table.Column<string>(type: "varchar(100)", nullable: false),
+                    status = table.Column<string>(type: "varchar(100)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PRIMARY", x => x.n);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tel1");
+        }
+    }
+}
