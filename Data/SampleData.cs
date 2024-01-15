6268581d6d9b4e800dc35350290c59636f43caa8
Mikhail Shashin <shashinma@icloud.com>
2024-01-15 19:00:40 +0300

    Replace IConfiguration with IOptions in SampleData

--- Data/SampleData.cs
+++ Data/SampleData.cs
@@ -1,36 +1,41 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 namespace PhoneEdit.Data;
 
 public class SampleData
 {
-    private static IConfiguration _configuration;
+    private readonly DefaultUserOptions _options;
 
-    public SampleData(IConfiguration configuration)
+    public SampleData(IOptions<DefaultUserOptions> optionsAccessor)
     {
-        _configuration = configuration;
+        _options = optionsAccessor.Value;
     }
-    // Creates default app's admin
+
     public static async Task CreateDefaultUser(IServiceProvider serviceProvider)
     {
         var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
-        var configuration = serviceProvider.GetRequiredService<IConfiguration>();
-        
-        var defaultUsername = configuration.GetValue<string>("DefaultUsername");
-        var defaultPassword = configuration.GetValue<string>("DefaultPassword");
+        var optionsAccessor = serviceProvider.GetRequiredService<IOptions<DefaultUserOptions>>();
+        var options = optionsAccessor.Value;
 
         var userCheck = await userManager.Users.AnyAsync();
         if (!userCheck)
         {
             var adminUser = new IdentityUser()
             {
-                UserName = Environment.GetEnvironmentVariable("USERNAME") ?? defaultUsername,
-                Email = Environment.GetEnvironmentVariable("USERNAME") ?? defaultUsername,
+                UserName = options.Username,
+                Email = options.Username,
                 EmailConfirmed = true
             };
 
-            await userManager.CreateAsync(adminUser, (Environment.GetEnvironmentVariable("PASSWORD") ?? defaultPassword) ?? throw new InvalidOperationException());
+            await userManager.CreateAsync(adminUser, options.Password ?? throw new InvalidOperationException());
         }
     }
+}
+
+public class DefaultUserOptions
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
 }
